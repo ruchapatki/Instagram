c9b729d462886fed5c19e830a7d28889f5083185
Rucha Patki <ruchapatki@fb.com>
2018-07-10 11:36:40 -0700

    Styled the login page to look like the real Instagram login page

--- Instagram/View Controllers/LoginViewController.m
+++ Instagram/View Controllers/LoginViewController.m
@@ -13,6 +13,7 @@
 
 @property (weak, nonatomic) IBOutlet UITextField *usernameField;
 @property (weak, nonatomic) IBOutlet UITextField *passwordField;
+@property (weak, nonatomic) IBOutlet UIButton *loginButton;
 
 
 @end
@@ -22,6 +23,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    self.loginButton.layer.cornerRadius = 7;
 }
 
 - (void)didReceiveMemoryWarning {
